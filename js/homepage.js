ec513ff5b3c0ec23e8e28517d33b359c89d4e1b2
Vishal Reddy <52229466+dvishalreddy@users.noreply.github.com>
2020-05-06 19:21:13 +0530

    Initial commit

---
+++ js/homepage.js
@@ -0,0 +1,6 @@
+import { createPopper } from '@popperjs/core';
+const popcorn = document.querySelector('#AC1');
+const tooltip = document.querySelector('#tooltipAC');
+createPopper(AC1, tooltipAC, {
+  placement: 'top',
+});
